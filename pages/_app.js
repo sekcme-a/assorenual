2c6e547cf1617dbdf70d5aac66ccf9a2b4da6793
eotodcp <eotodcp@gmail.com>
2022-09-30 19:35:10 +0900

    adsf

--- pages/_app.js
+++ pages/_app.js
@@ -38,26 +38,11 @@ function MyApp({ Component, pageProps }) {
   
   return (
     <UserContext.Provider value={userData}>
-      <Head>
-          <meta charSet="UTF-8" />
-          <link rel="icon" href="/favicon.ico" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <title>대한생활체육회</title>
-          <meta name="description" content="대한생활체육회 소개, 체육회 현황, 대회정보 및 자료제공" />
-          <meta property="og:title" content="대한생활체육회" />
-          <meta property="og:description" content="대한생활체육회 소개, 체육회 현황, 대회정보 및 자료제공"></meta>
-        
-          <meta httpEquiv="X-UA-Compatible" content="IE=edge"></meta>
-          <link rel="canonical" href="https://xn--vk1by6xrzecngs4l6obxj.com"></link>
-          <meta name="robots" content="index,follow"></meta>
-          <meta property="og:type" content="website"></meta>
-          <meta property="og:image" content="https://xn--vk1by6xrzecngs4l6obxj.com/public/logo-circle.png" />
-          <meta property="og:url" content="https://xn--vk1by6xrzecngs4l6obxj.com"></meta>
-          <meta name="keywords" content="대한생활체육회"/>
-          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
-          {/* <meta name="naver-site-verification" content="a25127b45ca7a3bdbc3769501f80f6e529deb6f3" /> */}
-          <meta name="naver-site-verification" content="fcd6939d5a2a544afe2de373e246bedd86d5702d" />
-          <meta name="google-site-verification" content="h0jRizviYwbetdIjDK5dX1iXSEiLWvRJ_JqBAnc7nfg" />
+      <Head>  
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge"></meta>
+        <link rel="canonical" href="https://xn--vk1by6xrzecngs4l6obxj.com"></link>
+        <meta name="robots" content="index,follow"></meta>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
       </Head>
             <Script
         src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=10143b83ffc3b3f9b4dfefb69908cb81&libraries=services,clusterer&autoload=false`}
