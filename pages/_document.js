55d014c7f923ebb09780cb43f6558dca7783cd01
송현서 <songmin21s@songhyeonseoui-MacBookPro.local>
2024-12-11 17:02:42 +0900

    asdf

--- pages/_document.js
+++ pages/_document.js
@@ -13,7 +13,7 @@ class MyDocument extends Document {
         <Head>
           <meta charSet="UTF-8" />
           <link passHref rel="icon" href="https://xn--vk1by6xrzecngs4l6obxj.com/favicon.ico" />
-          <meta name="naver-site-verification" content="fcd6939d5a2a544afe2de373e246bedd86d5702d" />
+          <meta name="naver-site-verification" content="c551c54c615fa8b95537e7834e49aa0c067c9978" />
           
         </Head>
         <body>
